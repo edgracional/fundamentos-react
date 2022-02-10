515e779c86d92522af3897ff7a216c36e3c8770a
Eder Gracional <Emailedergacional@hotmail.com>
2022-02-10 16:16:06 -0300

    19. Component App

---
+++ src/App.jsx
@@ -0,0 +1,49 @@
+import React from "react"
+
+/* fazer um importe relativo  Primeiro que vai ser referenciado dentro da div'*/
+import Primeiro from './components/basicos/Primeiro';
+/* este componente é baseado em uma função*/
+import ComParametro from './components/basicos/ComParametro';
+import Fragmento from './components/basicos/Fragmento';
+
+
+/* função Arrow function */
+// eslint-disable-next-line import/no-anonymous-default-export
+export default () => (
+   
+        
+        <div id='app'>
+            <h1>Fundamentos React 2</h1>
+            <Fragmento />
+            <ComParametro
+                titulo="Situação do /aluno"
+                aluno="Pedro"
+                nota={9.3} 
+                />
+            <Primeiro></Primeiro>
+
+
+        </div>
+    
+);
+
+
+
+/* Forma antiga
+export default function(props){
+    return(
+        <div id='app'> 
+        <h1>Fundamentos React 2</h1>
+        <Fragmento/>
+        <ComParametro 
+            titulo="Situação do /aluno" 
+            aluno="Pedro" 
+            nota={9.3}/>
+            <Primeiro></Primeiro>
+        
+
+    </div>
+    );
+} */
+
+        
--- src/index.js
+++ src/index.js
@@ -3,29 +3,13 @@ import './index.css'
 import ReactDom from 'react-dom'
 import React from 'react'
 
-/* fazer um importe relativo  Primeiro que vai ser referenciado dentro da div'*/
-import Primeiro from './components/basicos/Primeiro'
-/* este componente é baseado em uma função*/
-import ComParametro from './components/basicos/ComParametro'
-import Fragmento from './components/basicos/Fragmento'
+import App from './App'
 
 
 ReactDom.render(
-   /* Os componentes são referenciados dentro do jsx como tag <Primeiro></Primeiro>  */
-    /* As duas propriedades titulo e subtitulo serão passadas como parametro (props) para a 
-    função ComParametro */
-    /* Essa é a idéia do componentes você pode utilizar qauntas vezes for necessário  */
-    <div id='app'> 
-        <Primeiro></Primeiro>
-        <ComParametro 
-            titulo="Situação do /aluno" 
-            aluno="Pedro" 
-            nota={9.3}/>
-        <Fragmento/>
-
-    </div>,
+    <App/>,
+            
     document.getElementById('root')
 )
 
-/* pode ser dessar forma:
-ComParametro titulo="Segundo Componente" subtitulo="Muito legal"></ComParametro> */
+
