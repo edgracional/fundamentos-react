cb695f0d90a8474c3139d5bf937e9c41ce9c5677
Eder Gracional <Emailedergacional@hotmail.com>
2022-02-14 08:57:51 -0300

    20. Desafio némero aleatório

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from "react"
-
+// eslint-disable-next-line no-unused-vars
+import Aleatorio from "./components/basicos/Aleatorio";
 /* fazer um importe relativo  Primeiro que vai ser referenciado dentro da div'*/
 import Primeiro from './components/basicos/Primeiro';
 /* este componente é baseado em uma função*/
@@ -14,12 +15,13 @@ export default () => (
         
         <div id='app'>
             <h1>Fundamentos React 2</h1>
+            <Aleatorio min={1} max={60} />
             <Fragmento />
             <ComParametro
-                titulo="Situação do /aluno"
+                titulo="Situação do aluno"
                 aluno="Pedro"
                 nota={9.3} 
-                />
+            />
             <Primeiro></Primeiro>
 
 
