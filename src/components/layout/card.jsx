8f3a08c301124e416ed5c81ef84388cc19bbd227
Eder Gracional <Emailedergacional@hotmail.com>
2022-02-16 10:34:47 -0300

    24. Componentes Card #03

--- src/components/layout/card.jsx
+++ src/components/layout/card.jsx
@@ -4,9 +4,14 @@ import React from "react";
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default (props) => {
+
+    const cardStyle = {
+        backgroundColor: props.color || '#F00',
+        borderColor: props.com || '#F00',
+    }
     
     return (
-        <div className="card">
+        <div className="card" style={cardStyle}>
             <div className="Title">{props.titulo}</div>
             <div className="Content">
                 {props.children}
