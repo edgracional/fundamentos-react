077f9398e7e5873fd721e0e040084a4c6e789b09
Eder Gracional <Emailedergacional@hotmail.com>
2022-02-22 11:31:59 -0300

    36. Componente Controlado

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import React from "react"
 
 import Card from "./components/layout/card";
 
+import Input from './components/formulario/Input';
 import IndiretaPai from './components/comunicacao/IndiretaPai';
 import DiretaPai from './components/comunicacao/DiretaPai';
 import UsuarioInfo from './components/condicional/UsuarioInfo';
@@ -31,6 +32,10 @@ export default () => (
             <h1>Fundamentos React 2</h1>
                 <div className="Cards">
                 
+                    <Card titulo="#11 - Componente Controlado" color="#E45F56">
+                        <Input></Input>
+                    </Card>
+
                     <Card titulo="#10 - Comunicação Indireta" color="#8BAD39">
                         <IndiretaPai></IndiretaPai>
                     </Card>
